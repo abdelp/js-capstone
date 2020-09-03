141469a1b9f13c503fb2c30270a99fa82a09e18d
Abdel <juniorperezpy@gmail.com>
2020-09-03 12:52:03 -0400

    Update config for a RPG game

--- src/Config/config.js
+++ src/Config/config.js
@@ -2,7 +2,15 @@ import 'phaser';
 
 export default {
   type: Phaser.AUTO,
-  parent: 'phaser-example',
+  parent: 'content',
   width: 800,
-  height: 600
+  height: 600,
+  pixelArt: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
+    }
+  }
 };
