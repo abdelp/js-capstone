dc64ee406a618b62756b583c44b58ee65d786a33
Abdel <juniorperezpy@gmail.com>
2020-09-04 19:19:02 -0400

    Add initial EnemiesMenu class structure

---
+++ src/Objects/EnemiesMenu.js
@@ -0,0 +1,12 @@
+import Menu from './Menu';
+
+export default class EnemiesMenu extends Menu {
+  
+  constructor(x, y, scene) {
+    super(x, y, scene);        
+  }
+ 
+  confirm() {
+    this.scene.events.emit("Enemy", this.menuItemIndex);
+  }
+};
