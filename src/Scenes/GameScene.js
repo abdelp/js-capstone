f28f9152ba058d6ec0e5f8af864dcd69a4ba7ede
Abdel <juniorperezpy@gmail.com>
2020-09-04 19:30:29 -0400

    Add initial Message structure

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -60,7 +60,6 @@ export default class GameScene extends Phaser.Scene {
     const tiles = map.addTilesetImage('spritesheet', 'tiles');
     const grass = map.createStaticLayer('Grass', tiles, 0, 0);
     const decorations = map.createStaticLayer('Decorations', tiles, 0, 0);
-    // const enemies = map.filterObjects('Enemies', (enemy) => console.log(enemy));
     const enemies = map.objects[0].objects;
     let obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0);
 
