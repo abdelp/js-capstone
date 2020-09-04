acaa3416729af62cdbf9f2cf26b77803fc0bbc45
Abdel <juniorperezpy@gmail.com>
2020-09-04 16:21:44 -0400

    Update map with new spritesheet

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -59,6 +59,7 @@ export default class GameScene extends Phaser.Scene {
 
     const tiles = map.addTilesetImage('spritesheet', 'tiles');
     const grass = map.createStaticLayer('Grass', tiles, 0, 0);
+    const decorations = map.createStaticLayer('Decorations', tiles, 0, 0);
     let obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0);
 
     obstacles.setCollisionByExclusion([-1]);
@@ -110,7 +111,8 @@ export default class GameScene extends Phaser.Scene {
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
     this.cameras.main.startFollow(this.player);
     this.cameras.main.roundPixels = true; // avoid tile bleed
-
+    this.cameras.main.setZoom(2);
+    
     this.cursors = this.input.keyboard.createCursorKeys();
 
     // where the enemies will be
