b17c4dade74cc2d78d1d03abbe97868e6f65de40
Abdel <juniorperezpy@gmail.com>
2020-09-05 12:28:56 -0400

    Add scenes configuration

---
+++ src/Config/scenes.js
@@ -0,0 +1,21 @@
+import GameScene from './../Scenes/GameScene';
+import BattleScene from './../Scenes/BattleScene';
+import BootScene from './../Scenes/BootScene';
+import PreloaderScene from './../Scenes/PreloaderScene';
+import TitleScene from './../Scenes/TitleScene';
+import OptionsScene from './../Scenes/OptionsScene';
+import CreditsScene from './../Scenes/CreditsScene';
+import UIScene from './../Scenes/UIScene';
+
+const scenes = [
+  { key: 'Boot', sceneConfig: BootScene},
+  { key: 'Preloader', sceneConfig: PreloaderScene},
+  { key: 'Title', sceneConfig: TitleScene},
+  { key: 'Options', sceneConfig: OptionsScene},
+  { key: 'Credits', sceneConfig: CreditsScene},
+  { key: 'Game', sceneConfig: GameScene},
+  { key: 'Battle', sceneConfig: BattleScene},
+  { key: 'UIScene', sceneConfig: UIScene}
+];
+
+export default scenes;
