d4d5d8bf134cf180e11e61183762a624d8ed5b99
Abdel <juniorperezpy@gmail.com>
2020-09-05 17:53:25 -0400

    Remove confirm method

--- src/Objects/EnemiesMenu.js
+++ src/Objects/EnemiesMenu.js
@@ -5,8 +5,5 @@ export default class EnemiesMenu extends Menu {
   constructor(x, y, scene) {
     super(x, y, scene);        
   }
- 
-  confirm() {
-    this.scene.events.emit("Enemy", this.menuItemIndex);
-  }
+
 };
