eae723c87cc264437f002df60ceac2b48230afea
Abdel <juniorperezpy@gmail.com>
2020-09-05 15:23:30 -0400

    Add addScenes, loadAssets, and createAnims

--- src/Objects/Utilities.js
+++ src/Objects/Utilities.js
@@ -1,7 +1,25 @@
+function addScenes(game, arr) {
+  for(let i = 0; i < arr.length; i += 1) {
+    game.scene.add(arr[i].key, arr[i].sceneConfig);
+  }
+}
+
 function destroyObjs(arr) {
   for (let i = 0; i < arr.length; i++) {
     arr[i].destroy();
   }
 }
 
-export default destroyObjs;
+function loadAssets(scene, arr) {
+  for (let i = 0; i < arr.length; i++) {
+    scene.load[arr[i].type](arr[i].key, arr[i].path, arr[i].opts);
+  }
+}
+
+function createAnims(scene, arr) {
+  for (let i = 0; i < arr.length; i += 1) {
+    scene.anims.create(arr[i]);
+  }
+}
+
+export { addScenes, destroyObjs, loadAssets, createAnims };
