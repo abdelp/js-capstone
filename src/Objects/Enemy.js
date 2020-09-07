13a0cf8bc48375d18bbd16c04db951378dde6fa4
Abdel <juniorperezpy@gmail.com>
2020-09-07 11:57:10 -0400

    Update enemy's height and width

--- src/Objects/Enemy.js
+++ src/Objects/Enemy.js
@@ -3,7 +3,7 @@ import Unit from './Unit';
 export default class Enemy extends Unit {
   constructor(scene, x, y, texture, frame, type, hp, damage) {
     super(scene, x, y, texture, frame, type, hp, damage);
-    this.displayHeight = 100;
-    this.displayWidth = 100;
+    this.displayHeight = 80;
+    this.displayWidth = 80;
   }
 };
