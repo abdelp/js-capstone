e6ca6a19ac9c503775b950a0a1470a756ffa88bd
Abdel <juniorperezpy@gmail.com>
2020-09-07 20:34:00 -0400

    Update constructor to receive medicalKits as parameter

--- src/Objects/PlayerCharacter.js
+++ src/Objects/PlayerCharacter.js
@@ -1,9 +1,10 @@
 import Unit from './Unit';
 
 export default class PlayerCharacter extends Unit {
-  constructor(scene, x, y, texture, frame, type, hp, damage) {
+  constructor(scene, x, y, texture, frame, type, hp, medicalKits, damage) {
     super(scene, x, y, texture, frame, type, hp, damage);
     this.flipX = true;
     this.setScale(2);
+    this.medicalKits = medicalKits;
   }
 };
