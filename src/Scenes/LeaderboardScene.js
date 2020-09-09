ec04c1ebfaa5fcca8ff13327723ef9d078034456
Abdel <juniorperezpy@gmail.com>
2020-09-09 13:58:01 -0400

    Update to show top 10 players scores

--- src/Scenes/LeaderboardScene.js
+++ src/Scenes/LeaderboardScene.js
@@ -1,7 +1,7 @@
 import 'phaser';
-// import config from '../Config/config';
+import config from '../Config/config';
 import Button from '../Objects/Button';
-import * as Scores from './../Objects/scoresProvider';
+import { getScores, saveScore } from './../Objects/scoresProvider';
 
 export default class LeaderboardScene extends Phaser.Scene {
   constructor() {
@@ -9,53 +9,17 @@ export default class LeaderboardScene extends Phaser.Scene {
   }
 
   async create() {
-    const result = (await Scores.getScores()).data.result;
-    console.log(result);
-    // Scores.createGame('LOTR_PHASER_RPG')
-    // .then(result => {
-    //   console.log(result);
-    // })
-    // .catch(err => console.log(err));
-    // this.title = this.add.text(0, 0, 'leaderboard', { fontSize: '32px', fontStyle: 'bold', fill: '#fff' });
-    // this.zone = this.add.zone(config.width / 2, config.height / 2, config.width, config.height);
+    this.leaderboardTitle = this.add.text(300, 80, 'Leaderboard', { fontSize: '26px', fill: '#fff' });
 
-    // Phaser.Display.Align.In.Center(
-    //   this.title,
-    //   this.zone,
-    // );
+    this.scoreList = (await getScores()).data.result;
+    this.scoreList = this.scoreList.sort((a, b) => b.score - a.score);
 
-    // this.title.displayOriginY = 210;
+    const length = this.scoreList.length > 10 ? 11 : this.scoreList.length;
+  
+    for(let i = 0; i < length; i ++) {
+      this.add.text(300, 115 + (i*15), `${this.scoreList[i].user} ${this.scoreList[i].score}`, { color: 'white'});
+    }
 
-    // getScores()
-    //   .then(scores => {
-    //     const arr = [];
-    //     scores.map((user, i) => {
-    //       arr.push(
-    //         `${(i + 1).toString()}. ${user[1]}                      ${user[0].toString()}`,
-    //       );
-    //       return true;
-    //     });
-
-    //     const graphics = this.add.graphics();
-    //     graphics.fillRect(235, 133, 320, 340);
-
-    //     const mask = new Phaser.Display.Masks.GeometryMask(this, graphics);
-
-    //     const text = this.add.text(250, 150, arr, { fontFamily: 'Arial', color: '#fff', wordWrap: { width: 310 } }).setOrigin(0);
-
-    //     text.setMask(mask);
-
-    //     const zone = this.add.zone(235, 130, 320, 256).setOrigin(0).setInteractive();
-
-    //     zone.on('pointermove', (pointer) => {
-    //       if (pointer.isDown) {
-    //         text.y += (pointer.velocity.y / 10);
-
-    //         text.y = Phaser.Math.Clamp(text.y, -400, 300);
-    //       }
-    //     });
-    //   });
-
-    // this.menuButton = new Button(this, 400, 530, 'blueButton1', 'blueButton2', 'menu', 'Title');
+    this.menuButton = new Button(this, 400, 530, 'blueButton1', 'blueButton2', 'menu', 'Title');
   }
 }
