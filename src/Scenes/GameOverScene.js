5dd6452a347580a5e49fb7eb9db6e8db3897f6f8
Abdel <juniorperezpy@gmail.com>
2020-09-09 14:01:16 -0400

    Remove leaderboard

--- src/Scenes/GameOverScene.js
+++ src/Scenes/GameOverScene.js
@@ -9,17 +9,6 @@ export default class gameOverScene extends Phaser.Scene {
 
   async create (data) {
     this.gameFinishedText = this.add.text(300, 50, 'You have lost', { fontSize: '32px', fill: '#fff' });
-    this.leaderboardTitle = this.add.text(300, 80, 'Leaderboard', { fontSize: '26px', fill: '#fff' });
-
-    await ScoresProvider.saveScore(data.name, data.score);
-    this.scoreList = (await ScoresProvider.getScores()).data.result;
-    this.scoreList = this.scoreList.sort((a, b) => b.score - a.score);
-
-    const length = this.scoreList.length > 10 ? 11 : this.scoreList.length;
-  
-    for(let i = 0; i < length; i ++) {
-      this.add.text(300, 115 + (i*15), `${this.scoreList[i].user} ${this.scoreList[i].score}`, { color: 'white'});
-    }
 
     this.menuButton = new Button(this, 400, 530, 'blueButton1', 'blueButton2', 'menu', 'Title');
   }
